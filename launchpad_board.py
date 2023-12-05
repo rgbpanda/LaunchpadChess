5ea6668235b3834e9b420afe191c578f436dec90
randy <randy@randys-MacBook-Pro.local>
2023-12-04 20:56:07 -0500

    small fixes

--- launchpad_board.py
+++ launchpad_board.py
@@ -1,18 +1,13 @@
 import launchpad_py as launchpad
 import logging
-import time
 
 from helpers import touch_to_uci, uci_to_xy
 from pygame.time import wait
 
 logging.basicConfig(level=logging.INFO)
 
-BOARD_WHITE_COLOR = (200, 200, 22)
-BOARD_BLACK_COLOR = (0, 0, 200)
-
-YELLOW = (255, 255, 0)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
+BOARD_WHITE_COLOR = (10, 10, 5)
+BOARD_BLACK_COLOR = (0, 0, 10)
 
 
 class LaunchPadBoard:
@@ -35,24 +30,29 @@ class LaunchPadBoard:
 
     def reset(self):
         for space in self.modified_spaces:
-            space = touch_to_uci(space)
-            self._set_color_touch(space, self.default_colors[space])
+            self._set_color_uci(space, self.default_colors[space])
         self.modified_spaces = []
 
     def poll_for_event(self):
         events = self.lp.ButtonStateRaw(returnPressure=True)
-        press_event = events[1] > 0
-        # release_event = events[1] <= 0
-        if events != [] and events[0] != 255 and press_event:
+
+        # release_event is events[1] <= 0
+        if events != [] and events[0] != 255 and events[1] > 0:
             return touch_to_uci(events[0])
 
-    def light_green(self, uci):
+    def light_square(self, uci, color):
         self.modified_spaces.append(uci)
-        self._set_color_uci(uci, GREEN)
+        self._set_color_uci(uci, color)
 
-    def light_red(self, uci):
-        self.modified_spaces.append(uci)
-        self._set_color_uci(uci, RED)
+    # Chess square is a string representing the chess square i.e. "a8"
+    # Color is a tuple containing the RGB values to set that square
+    def _set_color_uci(self, uci, color):
+        x, y = uci_to_xy(uci)  # Try several times to be sure
+        self.lp.LedCtrlXY(x, y, color[0], color[1], color[2])
+        wait(5)
+        self.lp.LedCtrlXY(x, y, color[0], color[1], color[2])
+        wait(5)
+        self.lp.LedCtrlXY(x, y, color[0], color[1], color[2])
 
     def _set_default_lights(self):
         for x in range(1, 9):
@@ -61,19 +61,7 @@ class LaunchPadBoard:
                 uci = touch_to_uci(square_touch_event)
                 if (x % 2 == 0 and y % 2 == 0) or (x % 2 != 0 and y % 2 != 0):
                     self.default_colors[uci] = BOARD_BLACK_COLOR
-                    self._set_color_touch(square_touch_event, BOARD_BLACK_COLOR)
+                    self._set_color_uci(uci, BOARD_BLACK_COLOR)
                 else:
                     self.default_colors[uci] = BOARD_WHITE_COLOR
-                    self._set_color_touch(square_touch_event, BOARD_WHITE_COLOR)
-
-    # Chess square is a string representing the chess square i.e. "a8"
-    # Color is a tuple containing the RGB values to set that square
-    def _set_color_uci(self, uci, color):
-        x, y = uci_to_xy(uci)
-        self.lp.LedCtrlXY(x, y, color[0], color[1], color[2])
-
-    # Chess square is a string representing the chess square i.e. "11"
-    def _set_color_touch(self, touch_event, color):
-        x, y = uci_to_xy(touch_to_uci(touch_event))
-        self.lp.LedCtrlXY(x, y, color[0], color[1], color[2])
-        wait(2)
+                    self._set_color_uci(uci, BOARD_WHITE_COLOR)
--- main.py
+++ main.py
@@ -1,7 +1,44 @@
-from launchpad_board import LaunchPadBoard
+import chess
+import logging
 
-board = LaunchPadBoard()
+from stockfish import Stockfish
+from launchpad_board import LaunchPadBoard
+from chess import Move, Square
+
+launchpad = LaunchPadBoard()
+engine = Stockfish()
+board = chess.Board()
+
+board.legal_moves
+# board.reset()
+print(board)
+
+ORANGE = (255, 0, 0)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+
+def light_valid_moves(square):
+    for move in board.legal_moves:
+        move = str(move)
+        if str(move).startswith(event):
+            print(move[2:])
+            launchpad.light_square(move[2:], GREEN)
+
+
+last_event = None
 while True:
-    event = board.poll_for_event()
-    if event:
-        print(event)
+    event = launchpad.poll_for_event()
+
+    if event and event != last_event:
+        logging.info(f"Selected square is {event}")
+        launchpad.reset()
+        launchpad.light_square(event, ORANGE)
+        light_valid_moves(event)
+        last_event = event
+
+    elif event and event == last_event:
+        logging.info(f"Unselecting {last_event}")
+        launchpad.reset()
+        last_event = None
